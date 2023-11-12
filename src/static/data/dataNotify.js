27c2cc8d53d1fa52c24d23aa035bad3ff3756e44
rabeast <39444732+rabeast@users.noreply.github.com>
2023-11-12 18:08:07 +0300

    begin

---
+++ src/static/data/dataNotify.js
@@ -0,0 +1,83 @@
+export const staffPerson = [
+    { name: 'markov', fullName: 'Василий М.', userName: '@haltandcatch', selected: true, },
+    { name: 'meshkov', fullName: 'Александр М.', userName: '@okmeshkov', selected: true, },
+    { name: 'zalygin', fullName: 'Михаил З.', userName: '@mishanyazal', selected: true, },
+    { name: 'balamutin', fullName: 'Анатолий Б.', userName: '@dezm0d', selected: true, },
+    { name: 'davtyan', fullName: 'Виктор Д.', userName: '@crpt_davtyan', selected: true, },
+    { name: 'suprun', fullName: 'Иван С.', userName: '@lacronn', selected: true, },
+]
+
+export const dataNotify = {
+    notifyOps: [
+        {
+            alwaysSelected: true,
+            selected: true,
+            name: 'markov',
+            fullName: 'Василий Марков',
+            userName: '@haltandcatch'
+        },
+        {
+            alwaysSelected: true,
+            selected: true,
+            name: 'markmeshkovov',
+            fullName: 'Александр Мешков',
+            userName: '@okmeshkov'
+        },
+        {
+            alwaysSelected: true,
+            selected: true,
+            name: 'zalygin',
+            fullName: 'Михаил Залыгин',
+            userName: '@mishanyazal'
+        },
+        {
+            alwaysSelected: true,
+            selected: true,
+            name: 'balamutin',
+            fullName: 'Анатолий Баламутин',
+            userName: '@dezm0d'
+        },
+        {
+            alwaysSelected: false,
+            selected: false,
+            even: true,
+            name: 'davtyan',
+            fullName: 'Виктор Давтян',
+            userName: '@crpt_davtyan'
+        },
+        {
+            alwaysSelected: false,
+            selected: false,
+            even: false,
+            name: 'suprun',
+            fullName: 'Иван Супрун',
+            userName: '@lacronn'
+        },
+    ],
+
+    notifyStaff: [
+        {
+            alwaysSelected: true,
+            selected: true,
+            name: 'balamutin',
+            fullName: 'Анатолий Баламутин',
+            userName: '@dezm0d',
+        },
+        {
+            alwaysSelected: false,
+            selected: true,
+            even: true,
+            name: 'davtyan',
+            fullName: 'Виктор Давтян',
+            userName: '@crpt_davtyan'
+        },
+        {
+            alwaysSelected: false,
+            selected: true,
+            even: false,
+            name: 'suprun',
+            fullName: 'Иван Супрун',
+            userName: '@lacronn'
+        },
+    ],
+}
